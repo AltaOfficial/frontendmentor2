3f306d3de0c9b0081c66c2dec7d1c7b33bfa6241
jaedon farr <jaedonfarr@gmail.com>
2023-07-30 09:12:01 -0400

    Update main.js
    scores now change to what is in the json file

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-var data;
-const currentUrl = window.location;
+let data;
 
 (async () => {
-    await fetch('./data.json', {mode: 'no-cors', headers: {'Content-Type': 'application/json'}})
+    await fetch('./data.json', {mode: 'no-cors'})
     .then((response) => response.json())
     .then((json) => data = json);
 
     console.log(data);
+    document.getElementById("reaction-score").innerHTML = data[0]["score"] + " <span>/ 100</span>";
+    document.getElementById("memory-score").innerHTML = data[1]["score"] + " <span>/ 100</span>";
+    document.getElementById("verbal-score").innerHTML = data[2]["score"] + " <span>/ 100</span>";
+    document.getElementById("visual-score").innerHTML = data[3]["score"] + " <span>/ 100</span>";
 })();
