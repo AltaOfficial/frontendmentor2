4c04967ab583fad64c21c4375ff07083b8a3d691
jaedon farr <jaedonfarr@gmail.com>
2023-07-30 14:36:00 -0400

    updated readme

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-let data;
-
 (async () => {
+    let data;
     await fetch('./data.json', {mode: 'no-cors'})
     .then((response) => response.json())
     .then((json) => data = json);
 
-    console.log(data);
     document.getElementById("reaction-score").innerHTML = data[0]["score"] + " <span>/ 100</span>";
     document.getElementById("memory-score").innerHTML = data[1]["score"] + " <span>/ 100</span>";
     document.getElementById("verbal-score").innerHTML = data[2]["score"] + " <span>/ 100</span>";
